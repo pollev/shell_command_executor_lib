84003450cc6b19031278c401e09c15e30f16b86f
polle <vanhoofpolle@gmail.com>
2017-01-28 06:07:10 +0100

    added example command

---
+++ src/shellcommand/commands/ExampleCommand.java
@@ -0,0 +1,18 @@
+package shellcommand.commands;
+
+import shellcommand.Command;
+import shellcommand.CommandExecutor.OS;
+
+public class ExampleCommand extends Command{
+
+	@Override
+	public String getCommand() {
+		return "ls";
+	}
+
+	@Override
+	public OS getOSType() {
+		return OS.LINUX;
+	}
+
+}
